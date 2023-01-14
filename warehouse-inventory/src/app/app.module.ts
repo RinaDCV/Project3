38e1109ca180ffc4b3a3d2179c5c9e33007b641b
RinaDCV <115585596+RinaDCV@users.noreply.github.com>
2023-01-14 17:27:43 -0500

    merge to main for warehouse

--- warehouse-inventory/src/app/app.module.ts
+++ warehouse-inventory/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 // first import takes care of our ngforms and the second handles the form group and form buider
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from '../app-routing.module';
-import { AppComponent } from '../app.component';
-import { LoginComponent } from '../login/login.component';
-import { Warehouse1Component } from './warehouse1.component';
-import { MasterheaderComponent } from '../masterheader/masterheader.component';
-import { RegistrationComponent } from '../registration/registration.component';
-import { NavigationComponent } from '../navigation/navigation.component';
-import { FooterComponent } from '../footer/footer.component';
-import { AssociateComponent } from '../associate/associate.component';
-import { ManagerComponent } from '../manager/manager.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Warehouse1Component } from './warehouse1/warehouse1.component';
+import { MasterheaderComponent } from './masterheader/masterheader.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { AssociateComponent } from './associate/associate.component';
+import { ManagerComponent } from './manager/manager.component';
 
 
 @NgModule({
